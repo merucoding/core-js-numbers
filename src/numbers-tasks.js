451d1e7c28c8d744d5c123517b83103f0029b3e7
Meruert <mika.amantaikuzu@gmail.com>
2024-11-14 21:39:45 +0500

    feat: done toExponential, toFixed

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -350,8 +350,8 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
 
 /**
@@ -365,8 +365,8 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits);
 }
 
 /**
