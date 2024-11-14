be03ecc0f62ab620d9e9e62170e89a1418a96880
Meruert <mika.amantaikuzu@gmail.com>
2024-11-14 22:19:55 +0500

    feat: done roundToNearestInteger, getIntegerPartNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -519,8 +519,8 @@ function roundToLargestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
@@ -534,8 +534,8 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**
